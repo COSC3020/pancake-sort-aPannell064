5a139d1423e5f50376c028dc6385c37667ee7407
aPannell064 <adamkpannell@gmail.com>
2025-02-16 13:36:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,30 @@
 function flip(array, n) {
+    //len = arrray.length();
+    var flipper = [];
+    for(var i = 1; i < n+1 && array.length; i++) {
+        flipper.unshift(array.shift());
+    }
+
+    while(flipper.length) {
+        array.unshift(flipper.pop());
+    }
+
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    var maxIndex;
+    for (var i = array.length-1; i >= 0; i--) {
+        maxIndex = i;
+        for (var j = 0; j < i; j++) {
+            if(array[j] > array[maxIndex])
+            {
+                maxIndex = j;
+            }
+        }
+        array = flip(array, maxIndex+1);
+        array = flip(array, i+1);
+    }
     return array;
 }
